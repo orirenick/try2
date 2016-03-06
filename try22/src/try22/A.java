3df13012049c90f3f649a72af1a88fec2f339997
Ori <Ori@ROG>
2016-03-06 14:34:58 +0200

    Try To Pull

--- try22/src/try22/A.java
+++ try22/src/try22/A.java
@@ -4,8 +4,9 @@ public class A {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("i kill you");
+	
 		System.out.println("i'll kill u first");
+		System.out.println("Aviad try");
 	}
 
 }
